3c516ecd34bacd8701b106c1fd7df75693a9fab8
GitHub-Pawel <pawel.borzecin@gmail.com>
2020-05-24 22:42:04 +0200

    Open kismet website in the default browser

--- app/src/main/java/com/example/kn_project_app/KismetWebsite.java
+++ app/src/main/java/com/example/kn_project_app/KismetWebsite.java
@@ -2,13 +2,16 @@ package com.example.kn_project_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class KismetWebsite extends AppCompatActivity {
 
-    private WebView webView;
+//    private WebView webView;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,12 @@ public class KismetWebsite extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        webView=(WebView)findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("localhost:54321");
+//        webView=(WebView)findViewById(R.id.webView);
+//        webView.setWebViewClient(new WebViewClient());
+//        webView.loadUrl("http://localhost:54321");
+
+        uri = Uri.parse("http://localhost:54321"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
